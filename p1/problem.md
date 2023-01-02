4be46dad3d97e49b56e2c79844b5493b16acbef3
Yohan Jhaveri <yohanjhaveri@gmail.com>
2023-01-02 01:50:55 -0500

    Fixed markdown and spacing

--- p1/problem.md
+++ p1/problem.md
@@ -37,4 +37,4 @@ Output: [0,1]
 - Only one valid answer exists.
 
 
-Follow-up: Can you come up with an algorithm that is less than `O(n^2)` time complexity?
+**Follow-up:** Can you come up with an algorithm that is less than `O(n^2)` time complexity?
--- p2/solution.py
+++ p2/solution.py
@@ -9,9 +9,8 @@
 # Space Complexity - O(n)
 """
 
-
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val = 0, next = None):
         self.val = val
         self.next = next
 
@@ -22,11 +21,11 @@ def addTwoNumbers(l1, l2):
 
     while l1 or l2 or carry:
         total = 0
-        
+
         if l1:
             total += l1.val
             l1 = l1.next
-        
+
         if l2:
             total += l2.val
             l2 = l2.next

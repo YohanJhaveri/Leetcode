10247d209be6d019cc714732ab705fa3411fd374
Yohan Jhaveri <yohanjhaveri@gmail.com>
2023-01-27 19:57:11 -0500

    Solved p349

---
+++ p0349: Intersection of Two Arrays/solution.py
@@ -0,0 +1,13 @@
+"""
+# Runtime - 45 ms
+# Beats 88.83%
+
+# Memory - 14.1 MB
+# Beats 63.16%
+
+# Time Complexity - O(m + n)
+# Space Complexity - O(1)
+"""
+
+def intersection(nums1, nums2):
+    return set(nums1) & set(nums2)

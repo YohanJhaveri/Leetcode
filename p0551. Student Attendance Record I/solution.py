428987250cfddb8049c917507e86c1059fe52689
Yohan Jhaveri <yohanjhaveri@gmail.com>
2023-02-07 14:22:26 -0500

    Solved p551

---
+++ p0551. Student Attendance Record I/solution.py
@@ -0,0 +1,26 @@
+"""
+# Runtime - 31 ms
+# Beats 75.49%
+
+# Memory - 13.8 MB
+# Beats 94.74%
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
+"""
+
+def checkRecord(self, s: str) -> bool:
+    n = len(s)
+    a = 0
+
+    for i in range(n):
+        if i - 2 >= 0 and {s[i], s[i-1], s[i-2]} == {"L"}:
+            return False
+
+        if s[i] == "A":
+            a += 1
+
+        if a == 2:
+            return False
+
+    return True

574b1ad478e63cde775f3f41957be1c6965860c4
Yohan Jhaveri <yohanjhaveri@gmail.com>
2023-02-07 14:16:40 -0500

    Solved p543

---
+++ p0543: Diameter of a Binary Tree/solution.py
@@ -0,0 +1,28 @@
+"""
+# Runtime - 42 ms
+# Beats 89.43%
+
+# Memory - 16.3 MB
+# Beats 77.92%
+
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+"""
+
+def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    maximum = [0]
+
+    def maxDepth(node):
+        if not node:
+            return 0
+
+        L = maxDepth(node.left)
+        R = maxDepth(node.right)
+
+        maximum[0] = max(maximum[0], L + R)
+
+        return 1 + max(L, R)
+
+    maxDepth(root)
+
+    return maximum[0]
